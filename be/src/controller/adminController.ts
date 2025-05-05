1529c1aa401fd872fcb70e02dd3befcbeb09b0af
presabkhadka <presabkhadka30@gmail.com>
2025-05-06 01:48:45 +0545

    feat - endpoints for staff CRUD

--- be/src/controller/adminController.ts
+++ be/src/controller/adminController.ts
@@ -2,8 +2,14 @@ import { type Request, type Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import { Admin } from "../db/db";
-import { adminValidation } from "../validation/adminValidation";
+import { Admin, Staff } from "../db/db";
+import {
+  adminValidation,
+  staffValidation,
+} from "../validation/adminValidation";
+import path from "path";
+import fs from "fs";
+import { reduceEachTrailingCommentRange } from "typescript";
 
 dotenv.config();
 let JWT_SECRET = process.env.JWT_SECRET as string;
@@ -76,6 +82,131 @@ export async function adminSignup(req: Request, res: Response) {
       res.status(500).json({
         msg: error.message,
       });
+    }
+  }
+}
+
+export async function addStaff(req: Request, res: Response) {
+  try {
+    let parsedData = staffValidation.parse(req.body);
+    let { userName, userEmail, userDesignation, userExperience } = parsedData;
+    let staffImage = req.file ? `/uploads/${req.file.filename}` : null;
+
+    let existingStaff = await Staff.findOne({
+      userEmail,
+    });
+
+    if (existingStaff) {
+      if (req.file) {
+        const filePath = path.join(__dirname, "../uploads", req.file.filename);
+        fs.unlink(filePath, (err: any) => {
+          if (err) console.error("Error deleting file:", err);
+        });
+      }
+      res.status(409).json({
+        msg: "Staff with that email already exists in db",
+      });
+      return;
+    }
+
+    await Staff.create({
+      userName,
+      userEmail,
+      userDesignation,
+      userExperience,
+      staffImage,
+    });
+
+    res.status(200).json({
+      msg: "Staff created successfully",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        msg: error.message,
+      });
+    }
+  }
+}
+
+export async function deleteStaff(req: Request, res: Response) {
+  try {
+    let staffId = req.params.staffId;
+    if (!staffId) {
+      res.status(404).json({
+        msg: "No staff id in params",
+      });
+      return;
+    }
+
+    let existingStaff = await Staff.findOne({
+      _id: staffId,
+    });
+
+    if (!existingStaff) {
+      res.status(404).json({
+        msg: "No staff found with that id in db",
+      });
+      return;
+    }
+
+    await Staff.deleteOne({
+      _id: staffId,
+    });
+
+    res.status(200).json({
+      msg: "Staff deleted successfully",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        msg: error.message,
+      });
+    }
+  }
+}
+
+export async function editStaff(req: Request, res: Response) {
+  try {
+    let staffId = req.params.staffId;
+    if (!staffId) {
+      res.status(404).json({
+        msg: "No staff id in params",
+      });
+      return;
+    }
+
+    const { userName, userEmail, userDesignation, userExperience } = req.body;
+    const staffImage = req.file ? `/uploads/${req.file.filename}` : null;
+
+    const fieldsToUpdate: Record<string, any> = {};
+
+    if (userName) fieldsToUpdate.userName = userName;
+    if (userEmail) fieldsToUpdate.userEmail = userEmail;
+    if (userDesignation) fieldsToUpdate.userDesignation = userDesignation;
+    if (userExperience) fieldsToUpdate.userExperience = userExperience;
+    if (staffImage) fieldsToUpdate.staffImage = staffImage;
+
+    if (Object.keys(fieldsToUpdate).length === 0) {
+      res.status(200).json({
+        msg: "No fields to update",
+      });
+      return;
+    }
+
+    let result = await Staff.updateOne(
+      {
+        _id: staffId,
+      },
+      { $set: fieldsToUpdate }
+    );
+
+    res.status(200).json({
+      msg: "Staff updated successfully",
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      msg: error.message;
     }
   }
 }
