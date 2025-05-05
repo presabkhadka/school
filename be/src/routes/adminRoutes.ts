087d1fe1dfa012f0a30398fe34fd3ad311c25815
presabkhadka <presabkhadka30@gmail.com>
2025-05-06 01:48:53 +0545

    updt - routing the staff CRUD endpoints

--- be/src/routes/adminRoutes.ts
+++ be/src/routes/adminRoutes.ts
@@ -1,10 +1,53 @@
 import { Router, type Request, type Response } from "express";
 import AdminMiddleware from "../middleware/adminMiddleware";
-import { adminLogin, adminSignup } from "../controller/adminController";
+import {
+  addStaff,
+  adminLogin,
+  adminSignup,
+  deleteStaff,
+  editStaff,
+} from "../controller/adminController";
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../uploads"));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files are allowed!"));
+    }
+  },
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
 
 const adminRouter = Router();
 
 adminRouter.post("/login", adminLogin);
 adminRouter.post("/signup", adminSignup);
+adminRouter.post(
+  "/add-staff",
+  AdminMiddleware,
+  upload.single("staffImage"),
+  addStaff
+);
+adminRouter.delete("/delete-staff/:staffId", AdminMiddleware, deleteStaff);
+adminRouter.patch("/update-staff/:staffId", AdminMiddleware, editStaff);
 
 export { adminRouter };
