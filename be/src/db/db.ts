3e0493770c4b5465a7568028a7d164a0b8a2b504
presabkhadka <presabkhadka30@gmail.com>
2025-05-05 20:44:39 +0545

    updt - admin schema change

--- be/src/db/db.ts
+++ be/src/db/db.ts
@@ -1,12 +1,13 @@
 import mongoose, { mongo } from "mongoose";
 
 mongoose.connect(
-  "mongodb+srv://pchiranjivi2050:qOWJi2HDO8WhrGuZ@cpcluster.4apcyab.mongodb.net/"
+  "mongodb+srv://pchiranjivi2050:qOWJi2HDO8WhrGuZ@cpcluster.4apcyab.mongodb.net/balmandir"
 );
 
 const adminSchema = new mongoose.Schema({
   userName: String,
   userEmail: String,
+  userPassword: String,
   userContact: String,
 });
 
