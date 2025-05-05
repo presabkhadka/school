132c1d71af1a5d7de6092eecd3a8de47fbf79301
presabkhadka <presabkhadka30@gmail.com>
2025-05-06 01:46:49 +0545

    feat - multer in server level

--- be/src/server.ts
+++ be/src/server.ts
@@ -2,11 +2,14 @@ import express from "express";
 import cors from "cors";
 import { adminRouter } from "./routes/adminRoutes";
 import { userRouter } from "./routes/userRoutes";
+import path from "path";
 
 const app = express();
 const port = 4646;
 app.use(cors());
 app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/admin", adminRouter);
 app.use("/", userRouter);
