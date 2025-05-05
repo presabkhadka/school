c0cfc2240c4f4d0c13bf7a3d955c0e68010caaf6
presabkhadka <presabkhadka30@gmail.com>
2025-05-06 01:47:34 +0545

    updt - added validaton for staff

--- be/src/validation/adminValidation.ts
+++ be/src/validation/adminValidation.ts
@@ -9,3 +9,17 @@ export const adminValidation = z.object({
     .min(10, "Contact number should be 10 characters")
     .max(10, "Contact number cannot exceed more than 10 characters"),
 });
+
+export const staffValidation = z.object({
+  userName: z.string().min(1, "Username is required"),
+  userEmail: z.string().email("Invalid email format"),
+  userDesignation: z.string({
+    required_error: "Designation is required",
+    invalid_type_error: "Designation must be string",
+  }),
+  userExperience: z.coerce.number({
+    required_error: "Experience is required",
+    invalid_type_error: "Experience must be a number",
+  }),
+  staffImage: z.string().optional(),
+});
