623b8fad8ab6ee912795b09cb82d41117f43f658
presabkhadka <presabkhadka30@gmail.com>
2025-05-05 20:45:30 +0545

    updt - routing admin signup endpoint

--- be/src/routes/adminRoutes.ts
+++ be/src/routes/adminRoutes.ts
@@ -1,9 +1,10 @@
 import { Router, type Request, type Response } from "express";
 import AdminMiddleware from "../middleware/adminMiddleware";
-import { adminLogin } from "../controller/adminController";
+import { adminLogin, adminSignup } from "../controller/adminController";
 
 const adminRouter = Router();
 
 adminRouter.post("/login", adminLogin);
+adminRouter.post("/signup", adminSignup);
 
 export { adminRouter };
