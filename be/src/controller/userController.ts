348798fabddaaaca364cd5ab885283dc726d37e0
presabkhadka <presabkhadka30@gmail.com>
2025-05-07 09:00:41 +0545

    feat - get staff endpoint for user

---
+++ be/src/controller/userController.ts
@@ -0,0 +1,23 @@
+import { type Request, type Response } from "express";
+import { Staff } from "../db/db";
+
+export async function getStaff(req: Request, res: Response) {
+  try {
+    let staffs = await Staff.find({});
+    if (!staffs) {
+      res.status(404).json({
+        msg: "No staffs found in db",
+      });
+      return;
+    }
+    res.status(200).json({
+      staffs,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        msg: error.message,
+      });
+    }
+  }
+}
--- be/src/routes/userRoutes.ts
+++ be/src/routes/userRoutes.ts
@@ -1,5 +1,8 @@
 import { Router, type Request, type Response } from "express";
+import { getStaff } from "../controller/userController";
 
 const userRouter = Router();
+
+userRouter.get("/staffs", getStaff)
 
 export { userRouter };
