71bfb14627419a45cd12bce6807ee64ad1af5e91
presabkhadka <presabkhadka30@gmail.com>
2025-05-07 22:50:03 +0545

    feat - user endpoint for fetching gallery content

--- be/src/controller/userController.ts
+++ be/src/controller/userController.ts
@@ -1,5 +1,5 @@
 import { type Request, type Response } from "express";
-import { Notice, Staff } from "../db/db";
+import { Gallery, Notice, Staff } from "../db/db";
 
 export async function getStaff(req: Request, res: Response) {
   try {
@@ -34,6 +34,29 @@ export async function getNotice(req: Request, res: Response) {
 
     res.status(200).json({
       notice,
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({
+        msg: error.message,
+      });
+    }
+  }
+}
+
+export async function getGalery(req: Request, res: Response) {
+  try {
+    let gallery = await Gallery.find({});
+
+    if (!gallery) {
+      res.status(404).json({
+        msg: "No gallery content found in db",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      gallery,
     });
   } catch (error) {
     if (error instanceof Error) {
