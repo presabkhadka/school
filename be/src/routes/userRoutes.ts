0bb9327f9f507a25841fa72691983888668633d8
presabkhadka <presabkhadka30@gmail.com>
2025-05-07 22:50:37 +0545

    updt - routing user gallery endpoint

--- be/src/routes/userRoutes.ts
+++ be/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router, type Request, type Response } from "express";
-import { getNotice, getStaff } from "../controller/userController";
+import { getGalery, getNotice, getStaff } from "../controller/userController";
 
 const userRouter = Router();
 
 userRouter.get("/staffs", getStaff)
 userRouter.get("/notice", getNotice)
+userRouter.get("/gallery", getGalery)
 
 export { userRouter };
