e337238ac40dda7276c74dd9769e78978731cc39
presabkhadka <presabkhadka30@gmail.com>
2025-05-09 01:36:29 +0545

    updt - polling endpoint

--- fe/src/pages/view/school/AboutUs.tsx
+++ fe/src/pages/view/school/AboutUs.tsx
@@ -28,6 +28,8 @@ export default function AboutUs() {
       }
     };
     fetchStaffs();
+    let interval = setInterval(fetchStaffs, 10000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -45,7 +47,9 @@ export default function AboutUs() {
             />
           </div>
           <div className="flex flex-col w-1/3 gap-4">
-            <p className="text-2xl font-bold underline">A message from the principal</p>
+            <p className="text-2xl font-bold underline">
+              A message from the principal
+            </p>
             <p className="text-lg text-justify">
               Welcome to our official school website. As Principal, I am proud
               to lead an institution dedicated to academic excellence, character
@@ -63,9 +67,9 @@ export default function AboutUs() {
           </div>
         </div>
         <div className="flex justify-center px-8 py-10 gap-4 bg-muted dark:border">
-          <div className="grid grid-cols-1 md:grid md:grid-cols-3 gap-4">
-            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-blue-500">
-              <h2 className="font-bold text-3xl">Our Mission</h2>
+          <div className="grid grid-cols-1 md:grid md:grid-cols-3 gap-8">
+            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-blue-500 flex flex-col gap-2 hover:scale-105 transition-all duration-200">
+              <h2 className="font-bold text-3xl text-blue-500">Our Mission</h2>
               <p className="text-lg text-justify">
                 To provide high-quality, inclusive education that fosters
                 academic excellence, personal growth, and lifelong
@@ -73,16 +77,16 @@ export default function AboutUs() {
                 a safe and supportive environment.
               </p>
             </div>
-            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-green-500">
-              <h2 className="font-bold text-3xl">Our Vision</h2>
+            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-green-500 flex flex-col gap-2 hover:scale-105 transition-all duration-200">
+              <h2 className="font-bold text-3xl text-green-500">Our Vision</h2>
               <p className="text-lg text-justify">
                 To be a trusted leader in education, nurturing innovative,
                 responsible, and compassionate learners who positively
                 contribute to their communities and the world.
               </p>
             </div>
-            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-yellow-500">
-              <h2 className="font-bold text-3xl">Our Values</h2>
+            <div className="border-2 p-4 rounded-lg bg-white shadow-lg dark:bg-muted hover:border-yellow-500 flex flex-col gap-2 hover:scale-105 transition-all duration-200">
+              <h2 className="font-bold text-3xl text-yellow-500">Our Values</h2>
               <p className="text-lg text-justify">
                 Excellence, integrity, collaboration, inclusivity, and
                 innovation guide our commitment to providing quality education,
@@ -92,36 +96,42 @@ export default function AboutUs() {
             </div>
           </div>
         </div>
-        <div className="px-8 py-10 gap-4 flex flex-col justify-center items-center">
+        <div className="px-8 py-10 gap-8 flex flex-col justify-center items-center">
           <h1 className="text-4xl font-bold text-blue-500">Our Staff</h1>
-          <div className="grid grid-cols-1 md:grid md:grid-cols-3 gap-4">
+          <div className="grid grid-cols-1 md:grid md:grid-cols-3 gap-10">
             {staff.map((stf, index) => (
               <div
-                className="border-2 col-span-1 rounded-lg hover:border-blue-500"
+                className="border-2 col-span-1 rounded-lg hover:border-blue-500 hover:scale-105 transition-all duration-200 shadow-lg"
                 key={index}
               >
                 <img
                   src={`http://localhost:4646${stf.staffImage}`}
                   alt="teacher image"
-                  className="rounded-t-lg object-center aspect-video"
+                  className="rounded-t-lg object-cover h-92 w-full"
                 />
                 <div className="p-4 flex flex-col gap-2">
-                  <p className="text-xl font-bold">Name: {stf.userName}</p>
-                  <p className="text-xl font-semibold">
-                    Email: {stf.userEmail}
+                  <p className="text-xl font-bold">
+                    Name : <span className="font-normal">{stf.userName}</span>
                   </p>
                   <p className="text-xl font-semibold">
-                    Designtaion: {stf.userDesignation}
+                    Email : <span className="font-normal">{stf.userEmail}</span>
+                  </p>
+                  <p className="text-xl font-semibold">
+                    Designtaion :{" "}
+                    <span className="font-normal">{stf.userDesignation}</span>
                   </p>
                   <p className="font-semibold text-xl">
-                    Experience: {stf.userExperience} years
+                    Experience :{" "}
+                    <span className="font-normal">
+                      {stf.userExperience} years
+                    </span>
                   </p>
                 </div>
               </div>
             ))}
           </div>
         </div>
-      <Footer/>
+        <Footer />
       </div>
     </div>
   );
