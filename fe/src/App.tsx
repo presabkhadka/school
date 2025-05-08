b464d069f0c0b96a740a13c81ac9a67440297dd7
presabkhadka <presabkhadka30@gmail.com>
2025-05-08 23:24:29 +0545

    updt - implementing toaster in app.tsx and public photos

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Landing from "./pages/view/school/Landing";
 import { ThemeProvider } from "@/components/theme-provider";
+import AboutUs from "./pages/view/school/AboutUs";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
       <RouterProvider
-        router={createBrowserRouter([{ path: "/", element: <Landing /> }])}
+        router={createBrowserRouter([
+          { path: "/", element: <Landing /> },
+          { path: "/about", element: <AboutUs /> },
+        ])}
       ></RouterProvider>
+      <Toaster position="bottom-right" />
     </ThemeProvider>
   );
 }
