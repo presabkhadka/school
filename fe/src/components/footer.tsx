25102341be1280d55a968c1f1c062ada3f23e4da
presabkhadka <presabkhadka30@gmail.com>
2025-05-08 23:47:53 +0545

    feat - footer component created

---
+++ fe/src/components/footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="h-[10vh] flex justify-center items-center bg-black text-white border-t-2 dark:bg-muted">
+      <p>
+        Shree Balmandir Secondary School - © Copyright 2024. All Rights
+        Reserved.
+      </p>
+    </div>
+  );
+}
