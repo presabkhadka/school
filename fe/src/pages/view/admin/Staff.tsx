943d60107b2149dcb484c81606e5c7ad4fafba56
presabkhadka <presabkhadka30@gmail.com>
2025-05-11 03:48:16 +0545

    updt - responsiveness and delete functionality

---
+++ fe/src/pages/view/admin/Staff.tsx
@@ -0,0 +1,162 @@
+import AdminNavbar from "@/components/AdminNavbar";
+import { User } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import StaffForm from "@/components/StaffForm";
+import { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+export default function Staff() {
+  interface Staff {
+    _id: string;
+    userName: string;
+    userEmail: string;
+    userDesignation: string;
+    userExperience: string;
+    staffImage: string;
+  }
+
+  let [staffs, setStaffs] = useState<Staff[]>([]);
+  let [isSelected, setIsSelected] = useState<Staff | null>(null);
+
+  useEffect(() => {
+    let fetchStaff = async () => {
+      try {
+        let token = localStorage.getItem("Authorization")?.split(" ")[1];
+
+        if (!token) {
+          throw new Error("No token in authorization headers");
+        }
+
+        let response = await axios.get("http://localhost:4646/admin/staff", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        setStaffs(response.data.staff);
+      } catch (error) {
+        if (error instanceof Error) {
+          toast.error(error.message);
+        }
+      }
+    };
+    fetchStaff();
+    let interval = setInterval(fetchStaff, 10000);
+    return () => clearInterval(interval);
+  }, [staffs]);
+
+  let handleDelete = async (isSelected: any) => {
+    try {
+      let token = localStorage.getItem("Authorization")?.split(" ")[1];
+      if (!token) {
+        throw new Error("No token in headers");
+      }
+      let response = await axios.delete(
+        `http://localhost:4646/admin/delete-staff/${isSelected._id}`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      toast.success("Staff deleted successfully");
+    } catch (error) {
+      if (error instanceof Error) {
+        toast.error(error.message);
+      }
+    }
+  };
+
+  return (
+    <div className="h-screen w-screen grid grid-cols-1 md:grid-cols-12 dark:bg-muted overflow-y-scroll">
+      <div className="col-span-full">
+        <div className="top-0 sticky z-50">
+          <AdminNavbar />
+        </div>
+        <div className="p-6 flex flex-col gap-4">
+          <div className="flex justify-between items-center border-2 rounded-lg p-6 shadow-lg hover:border-blue-500">
+            <div className="flex items-center gap-2">
+              <User size={40} color="blue" />
+              <p className="text-3xl font-bold bg-gradient-to-r from-blue-500 to-blue-300 bg-clip-text text-transparent">
+                Staffs
+              </p>
+            </div>
+            <Dialog>
+              <DialogTrigger className="border px-4 py-2 rounded-lg bg-blue-500 text-white cursor-pointer hover:scale-105 hover:bg-blue-400">
+                Add Staff
+              </DialogTrigger>
+              <DialogContent>
+                <DialogHeader>
+                  <DialogTitle>Are you absolutely sure?</DialogTitle>
+                  <DialogDescription>
+                    <StaffForm />
+                  </DialogDescription>
+                </DialogHeader>
+              </DialogContent>
+            </Dialog>
+          </div>
+          <div className="w-full flex flex-col gap-4">
+            {staffs.map((staff, index) => (
+              <div
+                key={index}
+                className="border-2 rounded-lg hover:border-blue-500 flex flex-col sm:flex-row gap-4 shadow-lg"
+              >
+                <div className="w-full md:w-1/3">
+                  <img
+                    src={`http://localhost:4646${staff.staffImage}`}
+                    alt="staff image"
+                    className="aspect-video rounded-l-lg "
+                  />
+                </div>
+                <div className="flex flex-col sm:flex-row justify-between w-full p-4">
+                  <div className=" flex flex-col gap-4 ">
+                    <p className="font-bold">
+                      Name:{" "}
+                      <span className="font-normal">{staff.userName}</span>
+                    </p>
+                    <p className="font-bold">
+                      Email:{" "}
+                      <span className="font-normal">{staff.userEmail}</span>
+                    </p>
+                    <p className="font-bold">
+                      Designation:{" "}
+                      <span className="font-normal">
+                        {staff.userDesignation}
+                      </span>
+                    </p>
+                    <p className="font-bold">
+                      Experience:{" "}
+                      <span className="font-normal">
+                        {staff.userExperience}
+                      </span>
+                    </p>
+                  </div>
+                  <div className="self-end flex gap-2">
+                    <button className="px-4 py-2  bg-green-500 rounded-lg text-white hover:scale-105 cursor-pointer hover:bg-green-400">
+                      Edit
+                    </button>
+                    <button
+                      className="px-4 py-2  bg-red-500 rounded-lg text-white hover:scale-105 cursor-pointer hover:bg-red-400"
+                      onClick={() => {
+                        handleDelete(staff);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
